49123607c436e4224f431c7711331013517b5529
NaD <kh.nadine.x@gmail.com>
2026-02-24 03:56:24 +0200

    added the Wear OS Health Services

---
+++ SmartWatch/Watch App/app/build.gradle.kts
@@ -0,0 +1,88 @@
+import org.gradle.kotlin.dsl.implementation
+import java.util.Properties
+
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(localPropertiesFile.inputStream())
+}
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.example.MemoriaHomeWatch"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.MemoriaHomeWatch"
+        minSdk = 30
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            buildConfigField("String", "MQTT_USERNAME", "\"${localProperties["mqtt.username"]}\"")
+            buildConfigField("String", "MQTT_PASSWORD", "\"${localProperties["mqtt.password"]}\"")
+            buildConfigField("String", "MQTT_BROKER", "\"${localProperties["mqtt.broker"]}\"")
+        }
+        debug {
+            buildConfigField("String", "MQTT_USERNAME", "\"${localProperties["mqtt.username"]}\"")
+            buildConfigField("String", "MQTT_PASSWORD", "\"${localProperties["mqtt.password"]}\"")
+            buildConfigField("String", "MQTT_BROKER", "\"${localProperties["mqtt.broker"]}\"")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    useLibrary("wear-sdk")
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation(libs.play.services.wearable)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.compose.material)
+    implementation(libs.compose.foundation)
+    implementation(libs.wear.tooling.preview)
+    implementation(libs.activity.compose)
+    implementation(libs.core.splashscreen)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.material3)
+    implementation(libs.work.runtime.ktx)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
+
+    // paho library
+    implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
+
+    // Samsung Health Sensor SDK
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.aar"))))
+    // health services
+    implementation("androidx.health:health-services-client:1.1.0-beta01")
+    // ListenableFuture error
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.7.3")
+}
---
+++ SmartWatch/Watch App/app/src/main/java/com/example/MemoriaHomeWatch/presentation/TrackingActivity.kt
@@ -0,0 +1,201 @@
+package com.example.MemoriaHomeWatch.presentation
+
+import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.health.services.client.data.DataPointContainer
+import androidx.health.services.client.data.DataType
+import androidx.lifecycle.lifecycleScope
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Scaffold
+import androidx.wear.compose.material.Text
+import androidx.wear.tooling.preview.devices.WearDevices
+import com.samsung.android.service.health.tracking.data.DataPoint
+import com.samsung.android.service.health.tracking.data.HealthTrackerType
+import com.samsung.android.service.health.tracking.data.ValueKey
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+
+class TrackingActivity : ComponentActivity(), SensorEventListener {
+
+    companion object {
+        val TAG = "TrackActivityy"
+        fun dataHandle2(type: DataType<*, *>, data: DataPointContainer){
+            when (type){
+                DataType.HEART_RATE_BPM -> {
+                    val heartRatePoints = data.getData(DataType.HEART_RATE_BPM)
+                    if (heartRatePoints.isNotEmpty()) {
+                        val latest = heartRatePoints.last()
+                        Log.d(TAG, "HEART_RATE_BPM: ${latest.value}")
+                    } else {
+                        Log.d(TAG, "No heart rate data received")
+                    }
+                }
+                else -> {
+                    // add more for more sensors
+                    Log.d(TAG, "No heart rate data received")
+                }
+            }
+        }
+    }
+    private var isTrackingBPM = false
+    private var isTrakingAcclr = false
+    private var buttontext by mutableStateOf("Restart Tracking")
+    private var isTracking by mutableStateOf(false)
+    lateinit var healthSDKManager: HealthSDKManager
+    lateinit var healthServicesManager: HealthServicesManager
+    private lateinit var mSensorManager : SensorManager
+    private var offBodySensor : Sensor? = null
+    // trackers
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//        val intent = Intent(this, ForegroundService::class.java)
+
+//        healthSDKManager = HealthSDKManager(this,
+//            {startTracking()},
+//            {it.resolve(this)},
+//            {type, p0 -> dataHandle(type, p0)})
+
+        //healthSDKManager.connect()
+
+//        healthServicesManager = HealthServicesManager(this)
+//        healthServicesManager.startPassiveMonitoring(DataType.HEART_RATE_BPM, {type, data -> dataHandle2(type, data)}, false)
+
+        setContent {
+            MaterialTheme {
+                TrackAppUi(onExit = {buttonClicked()}, buttontext)
+            }
+        }
+    }
+
+
+
+    private fun dataHandle(type: HealthTrackerType, p0: List<DataPoint?>) {
+        lifecycleScope.launch(Dispatchers.Default) {
+            for (data in p0) {
+                data ?: continue
+
+                if(type == HealthTrackerType.HEART_RATE_CONTINUOUS){
+                    val hrData = data.getValue(ValueKey.HeartRateSet.HEART_RATE)
+                    launch(Dispatchers.Main) {
+                        // UI changes
+                        Log.d(TAG, "Heart Rate: $hrData")
+                    }
+                    continue
+                }
+                if (type == HealthTrackerType.ACCELEROMETER_CONTINUOUS){
+                    val accelDataX = data.getValue(ValueKey.AccelerometerSet.ACCELEROMETER_X)
+                    val accelDataY = data.getValue(ValueKey.AccelerometerSet.ACCELEROMETER_Y)
+                    val accelDataZ = data.getValue(ValueKey.AccelerometerSet.ACCELEROMETER_Z)
+                    launch(Dispatchers.Main) {
+                        Log.d(TAG, "Accelerometer X: $accelDataX, Accelerometer Y: $accelDataY, Accelerometer Z: $accelDataZ")
+                    }
+                    continue
+                }
+            }
+        }
+    }
+
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "TrackingActivity Destroyed")
+        if(::mSensorManager.isInitialized){ mSensorManager.unregisterListener(this); }
+        //healthSDKManager.disconnect()
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        //
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+        val offBodyDataFloat = p0?.values[0]
+        val offBodyData = offBodyDataFloat?.toInt()
+        if (offBodyData == 1){
+            Log.d(TAG, "Watch is being worn")
+            //healthSDKManager.resumeAllTrackers()
+        } else {
+            Log.d(TAG, "Watch is NOT being worn")
+            Toast.makeText(this, "Watch removed",Toast.LENGTH_LONG).show()
+            //healthSDKManager.pauseAllTrackers()
+        }
+    }
+
+    private fun startOffBodySensor(){
+        mSensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        offBodySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LOW_LATENCY_OFFBODY_DETECT)
+        mSensorManager.registerListener(this, offBodySensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    private fun startTracking() {
+        startOffBodySensor()
+        //healthSDKManager.startTracker(HealthTrackerType.ACCELEROMETER_CONTINUOUS)
+        //healthSDKManager.startTracker(HealthTrackerType.HEART_RATE_CONTINUOUS)
+        buttontext = "Stop Tracking"
+        isTracking = true
+    }
+
+    private fun buttonClicked(){
+        if(isTracking){
+            if(::mSensorManager.isInitialized){ mSensorManager.unregisterListener(this); }
+            //healthSDKManager.pauseAllTrackers()
+            buttontext = "start tracking"
+            isTracking = false
+        } else {
+            startOffBodySensor()
+            //healthSDKManager.resumeAllTrackers()
+            buttontext = "Stop Tracking"
+            isTracking = true
+        }
+    }
+}
+
+@Composable
+fun TrackAppUi(onExit: () -> Unit, buttontext: String) {
+    Scaffold {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(onClick = onExit, modifier = Modifier.size(100.dp)) {
+                Text(buttontext)
+            }
+        }
+    }
+}
+
+@Preview(
+    device = WearDevices.SMALL_ROUND,
+    showSystemUi = true,
+    backgroundColor = 0xff000000,
+    showBackground = true
+)
+@Composable
+fun TrackingActivityPreview() {
+    MaterialTheme {
+        TrackAppUi(onExit = {}, buttontext = "Stop Tracking")
+    }
+}
