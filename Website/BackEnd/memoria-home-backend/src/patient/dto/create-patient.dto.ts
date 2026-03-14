9601bce7cb608cc9513fddabe02c68a0a5e84f7c
DreamyThor <toussy12@gmail.com>
2026-03-14 15:38:22 +0200

    Edited .html files paths to accommodate new structure

--- Website/BackEnd/memoria-home-backend/src/patient/dto/create-patient.dto.ts
+++ Website/BackEnd/memoria-home-backend/src/patient/dto/create-patient.dto.ts
@@ -1,1 +1,7 @@
-export class CreatePatientDto {}
+export class CreatePatientDto {
+    patient_id: string
+    first_name: string
+    last_name: string
+    date_of_birth: Date
+    gender: 'Male' | 'Female'
+}
